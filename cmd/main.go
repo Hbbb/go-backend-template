8bcd5df9721435a2f9f9393b0a382f01281c2d9a
Harrison Borges <hborges005@gmail.com>
2023-12-23 15:20:12 -0700

    add repo boilerplate

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+// This package comes from internal/cmd/[api.go, worker.go, scheduler.go]
+// Each of those files exposes a cobra command that can be executed.
+// "github.com/hbbb/go-backend-template/cmd"
+
+func main() {
+	// ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	// defer cancel()
+
+	// ret := cmd.Execute(ctx)
+	// os.Exit(ret)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hbbb/go-backend-template
+
+go 1.21.0
